6b929207bdbbbec2913f51e6ab711d1dbab6c715
Souvik Banerjee <souvik.mlindia@gmail.com>
2021-05-23 17:57:38 +0100

    Day-20 Completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -13,6 +13,61 @@ class CartPage extends StatelessWidget {
         ),
         backgroundColor: Colors.transparent,
       ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
     );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SizedBox(
+        height: 200,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            "£9999".text.xl2.color(context.accentColor).make(),
+            ElevatedButton(
+              onPressed: () {},
+              child: "Buy".text.make(),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                context.theme.buttonColor,
+              )),
+            ).w32(context)
+          ],
+        ).p32(),
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.done),
+            title: "Item 1".text.make(),
+            trailing: IconButton(
+              icon: Icon(Icons.remove_circle_outline),
+              onPressed: () {},
+            ),
+          );
+        });
   }
 }
